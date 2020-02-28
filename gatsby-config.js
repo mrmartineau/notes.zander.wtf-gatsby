4badd954d508f260fefdd520006ab29a154b6ded
Zander Martineau <hi@zander.wtf>
2020-02-28 00:08:42 +0000

    chore: update pkg & config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Code notes',
-    description: 'Code notes',
+    title: 'notes.zander.wtf',
+    description: 'notes.zander.wtf',
     keywords: [],
     author: 'Zander'
   },
