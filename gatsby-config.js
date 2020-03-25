266a41762543e028e4bd515dfc9ae1c3c8cb6643
Zander Martineau <hi@zander.wtf>
2020-03-25 16:59:41 +0000

    add offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,25 @@ module.exports = {
         showThemeInfo: true,
         showDescriptionInSidebar: true
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Zander's Code Notes`,
+        short_name: `CodeNotes`,
+        description: `Notes on code. My memory bank.`,
+        start_url: `/`,
+        background_color: `hsl(210, 38%, 95%)`,
+        theme_color: `hsl(345, 100%, 69%)`,
+        display: `standalone`,
+        icon: `static/logo.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/*`, `/tag/*`]
+      }
     }
   ]
 }
